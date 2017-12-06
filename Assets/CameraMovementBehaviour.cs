8d8660014c556c0ca63d3db0f61627bc5811eace
Albert Dols Ferrandis <al315151@uji.es>
2017-12-06 01:25:52 +0100

    camera casi acabada
    Falta que gire sobre el punto adecuado, y que al hacer edge panning hacia delante y hacia atras funcione de forma relativa.

--- Assets/CameraMovementBehaviour.cs
+++ Assets/CameraMovementBehaviour.cs
@@ -12,9 +12,13 @@ public class CameraMovementBehaviour : MonoBehaviour {
 	float maxFieldOfView;
 
 	public float angleForce = 0.0f;
+	Quaternion rotationCamera;
+	public float cameraTraslationSpeed = 20f;
 
 	public GameObject limitPositionMinimumXZ;
 	public GameObject limitPositionMaximumXZ;
+
+	bool turnEnabled = false;
 
 	// Use this for initialization
 	void Start () 
@@ -22,8 +26,7 @@ public class CameraMovementBehaviour : MonoBehaviour {
 		print (cameraReference.name);
 
 		initialCameraLocation = cameraReference.transform;
-		maxFieldOfView = cameraReference.fieldOfView + 30;
-		
+		maxFieldOfView = cameraReference.fieldOfView + 30;		
 	}
 
 	void ZoomCameraControl()
@@ -52,49 +55,95 @@ public class CameraMovementBehaviour : MonoBehaviour {
 	{
 		if (Input.mousePosition.x > (Screen.width / 10) * 9) 
 		{
-			if (cameraReference.transform.position.x < limitPositionMaximumXZ.transform.position.x) 
-			{
-				cameraReference.transform.position = new Vector3 (cameraReference.transform.position.x + Time.deltaTime * 20, 
+			
+				/*cameraReference.transform.position = new Vector3 (cameraReference.transform.position.x + Time.deltaTime * 20, 
 					cameraReference.transform.position.y , cameraReference.transform.position.z);
-			}
+				*/
+			cameraReference.transform.position += cameraReference.transform.right * Time.deltaTime * cameraTraslationSpeed;
+
+
 		}
 		 else if (Input.mousePosition.x < (Screen.width / 10) * 1) 
 		{
-			if (cameraReference.transform.position.x > limitPositionMinimumXZ.transform.position.x) 
-			{
+			
+				/*
 				cameraReference.transform.position = new Vector3 (cameraReference.transform.position.x - Time.deltaTime * 20, 
 					cameraReference.transform.position.y , cameraReference.transform.position.z);
-			}
+				*/
+			cameraReference.transform.position -= cameraReference.transform.right * Time.deltaTime * cameraTraslationSpeed;
+
 		}
 		if (Input.mousePosition.y > (Screen.height / 10) * 9) 
 		{
-			if (cameraReference.transform.position.z < limitPositionMaximumXZ.transform.position.z) 
-			{
+				/*
 				cameraReference.transform.position = new Vector3 (cameraReference.transform.position.x, 
 					cameraReference.transform.position.y, cameraReference.transform.position.z + Time.deltaTime * 20);
-			}
+				*/
+				rotationCamera = cameraReference.transform.localRotation;
+
+				cameraReference.transform.rotation = new Quaternion (0f, cameraReference.transform.localRotation.y,
+													 cameraReference.transform.rotation.z, cameraReference.transform.rotation.w);
+				
+			cameraReference.transform.position += cameraReference.transform.forward * Time.deltaTime * cameraTraslationSpeed;
+
+				cameraReference.transform.rotation = rotationCamera;
+
 		}
 		 else if (Input.mousePosition.y < (Screen.height / 10) * 1) 
 		{
-			if (cameraReference.transform.position.z > limitPositionMinimumXZ.transform.position.z) 
-			{
+				/*
 				cameraReference.transform.position = new Vector3 (cameraReference.transform.position.x, 
 					cameraReference.transform.position.y, cameraReference.transform.position.z - Time.deltaTime * 20);
-			}
+				*/
+				rotationCamera = cameraReference.transform.localRotation;
+
+				cameraReference.transform.rotation = new Quaternion (0f, cameraReference.transform.localRotation.y,
+					cameraReference.transform.rotation.z, cameraReference.transform.rotation.w);
+
+			cameraReference.transform.position -= cameraReference.transform.forward * Time.deltaTime * cameraTraslationSpeed;
+
+				cameraReference.transform.rotation = rotationCamera;
+
 		}
-		
+
+		if (cameraReference.transform.position.z < limitPositionMinimumXZ.transform.position.z) 
+		{
+			cameraReference.transform.position = new Vector3 (cameraReference.transform.position.x, 
+				cameraReference.transform.position.y, limitPositionMinimumXZ.transform.position.z);
+		}
+		if (cameraReference.transform.position.z > limitPositionMaximumXZ.transform.position.z) 
+		{
+			cameraReference.transform.position = new Vector3 (cameraReference.transform.position.x, 
+				cameraReference.transform.position.y, limitPositionMaximumXZ.transform.position.z);
+		}
+		if (cameraReference.transform.position.x < limitPositionMinimumXZ.transform.position.x) 
+		{
+			cameraReference.transform.position = new Vector3 (limitPositionMinimumXZ.transform.position.x, 
+				cameraReference.transform.position.y , cameraReference.transform.position.z);
+		}
+		if (cameraReference.transform.position.x > limitPositionMaximumXZ.transform.position.x) 
+		{
+			cameraReference.transform.position = new Vector3 (limitPositionMaximumXZ.transform.position.x, 
+				cameraReference.transform.position.y , cameraReference.transform.position.z);
+		}
+
+
 	}
 
 	void RotateAroundCameraControl()
 	{
-		Node center = StageData.currentInstance.GetNodeFromPosition(cameraReference.ScreenToWorldPoint (
-													  new Vector3(Screen.width / 2, Screen.height / 2, 0f)));
+		if (turnEnabled) 
+		{
+			Node center = StageData.currentInstance.GetNodeFromPosition(cameraReference.ScreenToWorldPoint (
+				new Vector3(Screen.width / 2, Screen.height / 2, 0f)));
 
-		angleForce = Vector2.Distance(Input.mousePosition, new Vector2 (Screen.width / 2, Screen.height / 2));
-		print (angleForce);
+			angleForce = Mathf.Abs(Vector2.Distance(Input.mousePosition, new Vector2 (Screen.width / 2, Screen.height / 2))) / 200f;
+			//print (angleForce);
 
-		cameraReference.transform.RotateAround (center.position, new Vector3 (0f, 1.0f, 0.0f), angleForce);
+			if (Input.mousePosition.x < Screen.width / 2) {angleForce  = -angleForce;}
 
+			cameraReference.transform.RotateAround (center.position, new Vector3 (0f, 1.0f, 0.0f), angleForce);
+		}
 
 	}
 
@@ -104,12 +153,18 @@ public class CameraMovementBehaviour : MonoBehaviour {
 	void Update () {
 
 		ZoomCameraControl ();
-		PositionCameraControl ();
-		/*if (Input.GetMouseButtonDown (1)) 
+		RotateAroundCameraControl ();
+		if (Input.GetMouseButton(1)) 
 		{
-			RotateAroundCameraControl ();
+			turnEnabled = true;	
+		} 
+		else 
+		{
+			turnEnabled = false;	
+			PositionCameraControl ();
 		}
-		*/
+
+
 		//Testeo de que funciona mover la camara y tal...
 		/*cameraReference.gameObject.transform.position = new Vector3(cameraReference.gameObject.transform.position.x,
 			cameraReference.gameObject.transform.position.y + 1, cameraReference.gameObject.transform.position.z);*/
