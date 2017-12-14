e430435ddf390c50acaecfc58f65d2da3a7c59c5
al315171 <32774606+al315171@users.noreply.github.com>
2017-12-14 20:15:24 +0100

    Recolectar modificado again

--- Assets/Scripts/Acciones/Recolectar.cs
+++ Assets/Scripts/Acciones/Recolectar.cs
@@ -9,46 +9,28 @@ public class Recolectar : Accion {
     private void Awake()
     {
         m_Unidad = GetComponent<Unidad>();
-        Alcance = 1;
-        NodosAlAlcance = Control.GetNodosAlAlcance(m_Unidad.Nodo, Alcance);
     }
 
-	bool Ejecutar(Node n)
+	public bool Ejecutar()
     {
-        Unidad unidadActual = GetComponent<Unidad>();
-        Jugador jugador = Partida.GetPartidaActual().Jugadores[unidadActual.IdJugador];
-
-		if(NodosAlAlcance.Contains(n))
-			jugador.SumarRecursos(n.resourceType, /*n.cantidadRecolectada*/);
+		Jugador jugador = Partida.GetPartidaActual().Jugadores[m_Unidad.IdJugador];
+		jugador.SumarRecursos(n.resourceType, /*n.cantidadRecolectada*/);
 		return true;        
     }
 
 	public override void CancelarAccion()
 	{
-		m_Unidad.QuitarResaltoCasillasAlAlcance(NodosAlAlcance);
+		
 	}
 
 
 	public override void EmpezarAccion()
 	{
-		SeleccionarResaltoDeCasilla();
-		m_Unidad.ResaltarCasillasAlAlcance(NodosAlAlcance);
+		
 	}
 
 	public override void SeleccionarResaltoDeCasilla()
 	{
-		print("SeleccionarResaltoCasilla" + Alcance);
-		NodosAlAlcance = Control.GetNodosAlAlcance(m_Unidad.Nodo, Alcance);
-		for (int i = NodosAlAlcance.Count - 1; i >= 0; i--)
-		{
-			if (NodosAlAlcance[i].unidad != null ||
-				NodosAlAlcance[i].resourceType == TipoRecurso.NullResourceType)
-			{
-				print(NodosAlAlcance[i].unidad + "   " + NodosAlAlcance[i].resourceType + "  Eliminado");
-				NodosAlAlcance.Remove(NodosAlAlcance[i]);
-			}
-		}
-
-		m_Unidad.ResaltarCasillasAlAlcance(NodosAlAlcance);
+		
 	}
 }
